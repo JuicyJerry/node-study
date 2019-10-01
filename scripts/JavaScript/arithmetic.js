607c987bebef0720c50c5d0b265a2405b0127b40
JuicyJerry <ju2cyjerry@gmail.com>
2019-10-01 16:02:42 +0900

    값 입력 및 사칙연산 - 변수
    자바스크립트를 이용한 "값 입력 및 사칙연산 - 변수(input tag 활용)" 에 관한 실습 문제입니다.

---
+++ scripts/JavaScript/arithmetic.js
@@ -0,0 +1,28 @@
+var readInput1 = function() {
+    var input = document.getElementById("plus");
+    console.log(plus.value) === document.getElementById(num1.value) + document.getElementById(num2.value);
+}
+
+var readInput2 = function() {
+    var input = document.getElementById("minus");
+    console.log(minus.value) = parseInt(console.log("num1").value) - parseInt(console.log("num2").value);
+}
+
+var readInput3 = function() {
+    var input = document.getElementById("division");
+    console.log(division.value) = parseInt(console.log("num1").value) * parseInt(console.log("num2").value);
+}
+
+var readInput4 = function() {
+    var input = document.getElementById("mul");
+    console.log(mul.value) = parseInt(console.log("num1").value) / parseInt(console.log("num2").value);
+}
+
+// var readInput5 = function() {
+//     var input = document.getElementById("ans");
+//     console.log(plus.value);}
+
+
+
+// var btn1 = document.createElement("button");
+// var arithmetic = document.getElementById("arithmetic");
