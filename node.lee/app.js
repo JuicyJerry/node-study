eb4de755443434e6c5e2251559a878b960595e18
JuicyJerry <ju2cyjerry@gmail.com>
2019-10-23 18:31:05 +0900

    Node.js
    MySQL 연동 구현

--- node.lee/app.js
+++ node.lee/app.js
@@ -25,13 +25,25 @@ app.get('/main', function(req, res) {
 app.post('/email_post', function(req, res) {
     //get : req.param('email')
     console.log(req.body.email)
-    // res.send("<h1>welcom " + req.body.email + "</h1>")
     res.render('email.ejs', {'email' : req.body.email})
 })
 
 app.post('/ajax_send_email', function(req, res){
-    console.log(req.body.email);
-    //check validation about input value => select db
-    var responseData = {'result' : 'ok', 'email' : req.body.email}
-    res.json(responseData)
+    var email = req.body.email
+    var responseData = {};
+
+    var query = connection.query('select name from user where email="' + email + '"', function(err, rows) {
+        if(err) throw err;
+        if(rows[0]) {
+            console.log(row[0])
+            // responseData.result = "ok";
+            // responseData.name = rows[0].name;
+        } else {
+            console.log('none : ' + rows[0])
+        }
+    })
+
+    // res.json(responseData)
 })
+
+// https://www.interserver.net/tips/kb/troubleshoot-500-internal-server-error/
